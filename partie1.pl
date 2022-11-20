37e53a493f4c0ac26b75b7d66eceec41e70fb00b
Loona <loona.mac@gmail.com>
2022-11-20 20:51:10 +0100

    copie collage des concepts partie 1

---
+++ partie1.pl
@@ -0,0 +1,52 @@
+equiv(sculpteur,and(personne,some(aCree,sculpture))).
+equiv(auteur,and(personne,some(aEcrit,livre))).
+equiv(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livre)))).
+equiv(parent,and(personne,some(aEnfant,anything))).
+
+cnamea(personne).
+cnamea(livre).
+cnamea(objet).
+cnamea(sculpture).
+cnamea(anything).
+cnamea(nothing).
+
+cnamena(auteur).
+cnamena(editeur).
+cnamena(sculpteur).
+cnamena(parent).
+
+iname(michelAnge).
+iname(david).
+iname(sonnets).
+iname(vinci).
+iname(joconde).
+
+rname(aCree).
+rname(aEcrit).
+rname(aEdite).
+rname(aEnfant).
+
+inst(michelAnge,personne).
+inst(david,sculpture).
+inst(sonnets,livre).
+inst(vinci,personne).
+inst(joconde,objet).
+
+instR(michelAnge, david, aCree).
+instR(michelAnge, sonnets, aEcrit).
+instR(vinci, joconde, aCree).
+
+/* T-Box */
+[(sculpteur,and(personne,some(aCree,sculpture))),
+(auteur,and(personne,some(aEcrit,livre))),
+(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livr
+e)))),
+(parent,and(personne,some(aEnfant,anything)))]
+
+/* A-Box */
+[(michelAnge,personne), (david,sculpture), (sonnets,livre),
+(vinci,personne), (joconde,objet)]
+et l’autre qui contient les assertions de rôles :
+10
+[(michelAnge, david, aCree), (michelAnge, sonnet, aEcrit),(vinci,
+joconde, aCree)]
