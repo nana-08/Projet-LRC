8c50e760dac52a93d67aa08d8c03e9aac025cbd6
Loona <loona.mac@gmail.com>
2022-11-21 20:29:49 +0100

    j'y arrive pas

--- toDoPartie1.pl
+++ toDoPartie1.pl
@@ -12,6 +12,12 @@ concept(all(A, B)) :- setof(X, rname(X), Role), member(A, Role), concept(B).
 concept(not(A)) :- concept(A).
 concept(A) :- setof(X, cnamea(X), CAtom), member(A, CAtom).
 
-autoref.
-traitement_Tbox.
+autoref(Conc, Def, X) :- autoref(Conc, Def, X).
+autoref(Conc, _, Conc) :- write(user_error, "Concept auto-referant"), halt.
+autoref(Conc, _, X) :- X\==Conc.
+autoref(Conc, and(A, B), X) :- autoref(Conc, _, A), autoref(B).
+
+
+traitement_Tbox([]).
+traitement_Tbox([T|Q]) :- traitement_Tbox(Q).
 traitement_Abox.
