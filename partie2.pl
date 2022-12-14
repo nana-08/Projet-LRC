7d6cfe26094ce76bc1977e4658df35e4f2ff684e
giugiu <61977659+giugiu-stella@users.noreply.github.com>
2022-12-14 18:07:43 +0100

    mis en page plus belle
    mis en page plus belle

--- partie2.pl
+++ partie2.pl
@@ -11,8 +11,8 @@ suite(_,Abi,Abe,Tbox) :- nl, write('Cette reponse est incorrecte.'),nl,saisie_et
 
 acquisition_prop_type1(Abi,[(I, NotC)|Abi],_) :- nl, write("Entrez l'instance :"), nl, read(I), nl, write("Entrez le concept :"), nl, read(C),
                                         remplace(C, NewC), nnf(not(NewC), NotC),
-                                        write("--------- ON VEUT DEMONTRER LA PROPOSITION :  "),write(I),write(" : "),lisibilite(C),nl,
-                                        write("On ajoute à la ABox l'assertion suivante : "),nl,write(I),write(" : "),lisibilite(NotC),nl,nl.
+                                        write("--------- ON VEUT DEMONTRER LA PROPOSITION :  "),write(I),write(" : "),lisibilite(C),nl,nl,
+                                        write("On ajoute à la ABox l'assertion suivante : "),write(I),write(" : "),lisibilite(NotC),nl,nl.
 
 /* acquisition_prop_type2 */
 
@@ -22,7 +22,7 @@ concatene([],L1,L1).
 concatene([X|Y],L1,[X|L2]) :- concatene(Y,L1,L2).
 
 genere(Nom) :- compteur(V),nombre(V,L1),concatene([105,110,115,116],L1,L2),V1 is V+1,dynamic(compteur/1),retract(compteur(V)),
-        dynamic(compteur/1),assert(compteur(V1)),nl,nl,nl,name(Nom,L2).
+        dynamic(compteur/1),assert(compteur(V1)),name(Nom,L2).
 
 nombre(0,[]).
 nombre(X,L1) :- R is (X mod 10), Q is ((X-R)//10), chiffre_car(R,R1), char_code(R1,R2), nombre(Q,L), concatene(L,[R2],L1).
@@ -40,7 +40,7 @@ chiffre_car(9,'9').
 
 acquisition_prop_type2(Abi,[(I, C1etC2)|Abi],_):- genere(I), nl, write("Entrez C1 :"), nl, read(C1), nl, write("Entrez C2 :"), nl, read(C2),
                                             remplace(C1, NewC1), remplace(C2, NewC2), nnf(and(NewC1, NewC2), C1etC2),nl,nl,
-                                            write("--------- ON VEUT DEMONTRER LA PROPOSITION : "),write("("),lisibilite(C1),write(") ⊓ ("),lisibilite(C2),write(") ⊑ ⊥"),nl,
+                                            write("--------- ON VEUT DEMONTRER LA PROPOSITION : "),write("("),lisibilite(C1),write(") ⊓ ("),lisibilite(C2),write(") ⊑ ⊥"),nl,nl,
                                             write("On ajoute à la ABox l'assertion suivante : "),nl,write(I),write(" : "),lisibilite(C1etC2),nl,nl.
 
 
--- partie3.pl
+++ partie3.pl
@@ -7,16 +7,16 @@ tri_Abox([(I, or(C1,C2))|Q],Lie,Lpt,Li,[(I, or(C1,C2))|Lu],Ls) :- tri_Abox(Q,Lie
 tri_Abox([A|Q],Lie,Lpt,Li,Lu,[A|Ls]) :- tri_Abox(Q,Lie,Lpt,Li,Lu,Ls).
 
 /* --------- resolution --------- */
-resolution(Lie,Lpt,Li,Lu,Ls,Abr) :- write("--------- PASSAGE DANS LE NOEUD ∃ ---------"),nl,complete_some(Lie,Lpt,Li,Lu,Ls,Abr),
-                                    write("--------- PASSAGE DANS LE NOEUD ⊓ ---------"),nl,transformation_and(Lie,Lpt,Li,Lu,Ls,Abr),
-                                    write("--------- PASSAGE DANS LE NOEUD ∀ ---------"),nl,deduction_all(Lie,Lpt,Li,Lu,Ls,Abr),
-                                    write("--------- PASSAGE DANS LE NOEUD ⊔ ---------"),nl,transformation_or(Lie,Lpt,Li,Lu,Ls,Abr),
+resolution(Lie,Lpt,Li,Lu,Ls,Abr) :- nl,write("------------------------"),nl,write("PASSAGE DANS LE NOEUD ∃"),nl,write("------------------------"),nl,nl,complete_some(Lie,Lpt,Li,Lu,Ls,Abr),
+                                   nl,write("------------------------"),nl,write("PASSAGE DANS LE NOEUD ⊓"),nl,write("------------------------"),nl,nl,transformation_and(Lie,Lpt,Li,Lu,Ls,Abr),
+                                    nl,write("------------------------"),nl,write("PASSAGE DANS LE NOEUD ∀"),nl,write("------------------------"),nl,nl,deduction_all(Lie,Lpt,Li,Lu,Ls,Abr),
+                                    nl,write("------------------------"),nl,write("PASSAGE DANS LE NOEUD ⊔"),nl,write("------------------------"),nl,nl,transformation_or(Lie,Lpt,Li,Lu,Ls,Abr),
                                     nl,write("Aucun clash n'a été détecté. La proposition initiale n'a pas pu être démontrée."),nl,
                                     write("############ ECHEC ############"),nl,abort.
 
 /* --------- complete_some --------- */
 complete_some([],_,_,_,_,_).
-complete_some([(A,some(R,C))|Q],Lpt,Li,Lu,Ls,Abr) :- write("# On considère l'assertion  "),write(A),write(" : "),lisibilite(some(R,C)),nl,genere(B), 
+complete_some([(A,some(R,C))|Q],Lpt,Li,Lu,Ls,Abr) :-write("# On considère l'assertion  "),write(A),write(" : "),lisibilite(some(R,C)),nl,genere(B),
                                             evolue((B,C), Q, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
                                             affiche_evolution_Abox(Ls, [(A,some(R,C))|Q], Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, [(A,B,R)|Abr]),
                                             resolution(Lie1,Lpt1,Li1,Lu1,Ls1,[(A,B,R)|Abr]).  
@@ -72,21 +72,21 @@ evolue((I, some(R,C)), Lie, Lpt, Li, Lu, Ls, [(I,some(R,C))|Lie], Lpt, Li, Lu, L
 evolue((I, all(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, [(I, all(R,C))|Lpt], Li, Lu, Ls) :- clash((I, all(R,C)),Lpt).
 evolue((I, and(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, [(I, and(R,C))|Li], Lu, Ls) :- clash((I, and(R,C)),Li).
 evolue((I, or(R,C)), Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, [(I, or(R,C))|Lu], Ls) :- clash((I, or(R,C)),Lu).
-evolue(A, Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, [A|Ls]) :- write("passage pour Ls "),write(A),nl,clash(A,Ls).
+evolue(A, Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, [A|Ls]) :- clash(A,Ls).
 
 /* --------- affiche_evolution_Abox --------- */
-affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr1) :- write("# ETAT DE LA ABOX AVANT INSERTION :"), nl,
-                                            write("Ls = ["),lisibiliteListe(Ls),write("] ; "),
-                                            write("Lie = ["),lisibiliteListe(Lie),write("] ; "),
-                                            write("Lpt = ["),lisibiliteListe(Lpt),write("] ; "),
-                                            write("Li = ["),lisibiliteListe(Li),write("] ; "),
-                                            write("Lu = ["),lisibiliteListe(Lu),write("] ; "),
+affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr1) :- nl,nl,write("--------- # ETAT DE LA ABOX AVANT INSERTION : ---------"),nl,nl,
+                                            write("Ls = ["),lisibiliteListe(Ls),write("] ; "),nl,
+                                            write("Lie = ["),lisibiliteListe(Lie),write("] ; "),nl,
+                                            write("Lpt = ["),lisibiliteListe(Lpt),write("] ; "),nl,
+                                            write("Li = ["),lisibiliteListe(Li),write("] ; "),nl,
+                                            write("Lu = ["),lisibiliteListe(Lu),write("] ; "),nl,
                                             write("Abr = ["),lisibiliteAbr(Abr),write("] "),nl,nl,
-                                            write("# ETAT DE LA ABOX APRES INSERTION :"), nl,
-                                            write("Ls = ["),lisibiliteListe(Ls1),write("] ; "),
-                                            write("Lie = ["),lisibiliteListe(Lie1),write("] ; "),
-                                            write("Lpt = ["),lisibiliteListe(Lpt1),write("] ; "),
-                                            write("Li = ["),lisibiliteListe(Li1),write("] ; "),
-                                            write("Lu = ["),lisibiliteListe(Lu1),write("] ; "),
+                                            write("--------- # ETAT DE LA ABOX APRES INSERTION : ---------"),nl,nl,
+                                            write("Ls = ["),lisibiliteListe(Ls1),write("] ; "),nl,
+                                            write("Lie = ["),lisibiliteListe(Lie1),write("] ; "),nl,
+                                            write("Lpt = ["),lisibiliteListe(Lpt1),write("] ; "),nl,
+                                            write("Li = ["),lisibiliteListe(Li1),write("] ; "),nl,
+                                            write("Lu = ["),lisibiliteListe(Lu1),write("] ; "),nl,
                                             write("Abr = ["),lisibiliteAbr(Abr1),write("]"),nl,nl.
 
